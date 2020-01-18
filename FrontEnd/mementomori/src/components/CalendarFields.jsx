9657ea819af87a57351f16ab2a4b61bff053ea2d
romanpastu <roman.pastushenko.1997@gmail.com>
2020-01-18 16:03:43 +0100

    active calendarfields are now clickable

--- FrontEnd/mementomori/src/components/CalendarFields.jsx
+++ FrontEnd/mementomori/src/components/CalendarFields.jsx
@@ -4,20 +4,34 @@ import './CalendarFields.css'
 class CalendarFields extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            fieldId : ""
+        };
         console.log(props) //week number, beginning with 0
-
+        this.fieldClick = this.fieldClick.bind(this);
     }
 
+    componentDidMount(){
+        this.setState({
+            fieldId: this.props.weekId,
+        });
+    }
+
+    fieldClick(event){
+        console.log("pulsable");
+    }
 
     render() {
         var weekId = this.props.weekId;
+        //current week, round up
+        //lived weeks, round down
+        //total weeks, round up
         if (weekId < this.props.livedWeeks) {
             return <div id={weekId} className="cube-lived"></div>
         } else if (weekId == this.props.currentWeek) {
-            return <div id={weekId} title={weekId} className="cube green"> </div>
+            return <div id={weekId} title={weekId} className="cube green" onClick={this.fieldClick}> </div>
         } else {
-            return <div id={weekId} title={weekId} className="cube white"> </div>
+            return <div id={weekId} title={weekId} className="cube white" onClick={this.fieldClick}> </div>
         }
 
     }
