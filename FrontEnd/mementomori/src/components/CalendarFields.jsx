b433fc3d6d42cb106fd5526797a00711056f8584
romanpastu <roman.pastushenko.1997@gmail.com>
2020-01-18 15:30:43 +0100

    week display algo refactoring

--- FrontEnd/mementomori/src/components/CalendarFields.jsx
+++ FrontEnd/mementomori/src/components/CalendarFields.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import './CalendarFields.css'
 
-class CalendarFields extends React.Component{
+class CalendarFields extends React.Component {
     constructor(props) {
         super(props);
 
-      console.log(props) //week number, beginning with 0
-    
+        console.log(props) //week number, beginning with 0
+
     }
 
 
-    render(){
+    render() {
         var weekId = this.props.weekId;
-        if(weekId <= this.props.livedWeeks){
-            if(weekId == this.props.currentWeek){
-                return <div id={weekId} title={weekId} className="cube green"> </div>
-            }else{
-                return <div id={weekId} className="cube-lived"></div>
-            }
-        }else if(weekId == this.props.currentWeek){
-            
-                return <div id={weekId} title={weekId} className="cube green"> </div>
-            
-        }else{
+        if (weekId < this.props.livedWeeks) {
+            return <div id={weekId} className="cube-lived"></div>
+        } else if (weekId == this.props.currentWeek) {
+            return <div id={weekId} title={weekId} className="cube green"> </div>
+        } else {
             return <div id={weekId} title={weekId} className="cube white"> </div>
         }
-        
+
     }
 }
 export default CalendarFields;
