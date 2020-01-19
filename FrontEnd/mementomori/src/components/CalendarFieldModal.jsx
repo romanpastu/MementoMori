210edc5a09cb72ac126844c3038cb9857d2439b4
romanpastu <roman.pastushenko.1997@gmail.com>
2020-01-19 12:56:03 +0100

    now clicking calendar fields will render an element

---
+++ FrontEnd/mementomori/src/components/CalendarFieldModal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './CalendarFieldModal.css'
+class CalendarFieldModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+    }
+
+
+    render() {
+        return (
+            <div className="cube-to-spawn"></div>
+        )
+    }
+}
+
+export default CalendarFieldModal;
--- FrontEnd/mementomori/src/components/CalendarFields.jsx
+++ FrontEnd/mementomori/src/components/CalendarFields.jsx
@@ -1,38 +1,64 @@
 import React from 'react'
 import './CalendarFields.css'
+import CalendarFieldModal from './CalendarFieldModal'
 
 class CalendarFields extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            fieldId : ""
+            fieldId: "",
+            rating: -1,
+            description: "",
+            showModal: false
         };
         console.log(props) //week number, beginning with 0
-        this.fieldClick = this.fieldClick.bind(this);
+        this.showModal = this.showModal.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             fieldId: this.props.weekId,
         });
     }
 
-    fieldClick(event){
-        console.log("pulsable");
+
+    showModal() {
+        console.log(this.state.showModal)
+        
+
+        this.setState({
+            showModal: true
+        }, () => {
+            console.log(this.state.showModal)
+        });
+        
+        
+        console.log("clicked show modal")
+    }
+
+
+    closeModal() {
+        this.setState({ showModal: false });
     }
 
     render() {
         var weekId = this.props.weekId;
+        
         //current week, round up
         //lived weeks, round down
         //total weeks, round up
         if (weekId < this.props.livedWeeks) {
             return <div id={weekId} className="cube-lived"></div>
         } else if (weekId == this.props.currentWeek) {
-            return <div id={weekId} title={weekId} className="cube green" onClick={this.fieldClick}> </div>
+            return <div id={weekId} title={weekId} className="cube green" onClick={this.showModal}> 
+                  { this.state.showModal ? <CalendarFieldModal /> : null}
+            </div>
         } else {
-            return <div id={weekId} title={weekId} className="cube white" onClick={this.fieldClick}> </div>
+            return <div id={weekId} title={weekId} className="cube white" onClick={this.showModal}>
+                { this.state.showModal ? <CalendarFieldModal /> : null}
+                 </div>
         }
+            
 
     }
 }
