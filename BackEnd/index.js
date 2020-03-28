d7c5d91afd860763215e39f2b0d33398808b293a
romanpastu <roman.pastushenko.1997@gmail.com>
2020-03-28 12:03:15 +0100

    register concept

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,12 +1,21 @@
 
 let constants = require('./constants.js')
+
 //express use settings
 const express = require("express");
 const app = express();
 
+
+//express settings
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //db settings
 const pgp = require("pg-promise")();
 const db = pgp(constants.dbUrl)
+
+//requires for jwt management and auth
+const { hash, compare } = require('bcryptjs')
 
 //express calls
 app.get("/hello", (req, res) => {
@@ -23,6 +32,31 @@ app.post("/test", (req, res) =>{
     
 })
 
+//User register related queries
+
+app.post('/register', async(req,res) => {
+
+  const {email,password} = req.body;
+
+  const hashedPassword = await hash(password,10);
+  db.query("INSERT INTO USERS (email, password) VALUES ('" + email + "','" + hashedPassword + "')").then(function (data) {
+    db.query("SELECT id from users where email='" + email + "';").then(function (data) {
+      db.query("INSERT INTO user_permissions values ('" + data[0].id + "', 'true')").then(function(data){
+        res.send("inserted");
+        console.log("inserted")
+      }).catch(function (error) {
+        console.log("ERROR: ", error)
+        res.send("error")
+    });
+    }).catch(function (error) {
+      console.log("ERROR: ", error)
+      res.send("error")
+  });
+  }).catch(function (error) {
+    console.log("ERROR: ", error)
+    res.send("error")
+});
+})
 
 //Express port
 app.listen(1234, () => {
