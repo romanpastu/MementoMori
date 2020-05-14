f7922a4a0187c7e65864856bce18c8a2cb645c86
romanpastu <roman.pastushenko.1997@gmail.com>
2020-05-14 19:35:12 +0200

    added admin panel to navbar

--- FrontEnd/mementomori/src/components/Navbar.jsx
+++ FrontEnd/mementomori/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPowerOff, faUser, faColumns, faChartLine, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import { faPowerOff, faUser, faColumns, faChartLine, faCalendar , faUserCog} from '@fortawesome/free-solid-svg-icons'
 import { Redirect } from 'react-router'
 import Cookies from 'js-cookie';
+import {isAdmin} from '../services/userInfo.js'
 import './Navbar.css'
 
 export default class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            admin: false
         }
         this.logout = this.logout.bind(this)
+    }
+
+    componentDidMount(){
+        if(isAdmin()){
+            this.setState({
+                admin:true
+            }, () =>{
+                console.log(this.state.admin)
+            })
+        }
     }
 
     logout() {
@@ -26,6 +38,7 @@ export default class Navbar extends Component {
             <div className="appNavbar">
                 <div className="navEl nav1"><FontAwesomeIcon icon={faCalendar} className="navIcon" onClick={() => this.props.history.push("/dashboard")}/></div>
                 <div className="navEl nav4"><FontAwesomeIcon icon={faChartLine} className="navIcon" onClick={() => this.props.history.push("/stats")}/></div>
+                {this.state.admin ? <div className="navEl nav5"><FontAwesomeIcon icon={faUserCog} className="navIcon" onClick={() => this.props.history.push("/admin")}/></div> : null}
                 <div className="navEl nav2"><FontAwesomeIcon icon={faUser} className="navIcon" /></div>
                 <div className="navEl nav3"><FontAwesomeIcon icon={faPowerOff} className="navIcon" onClick={this.logout}/></div>
             </div>
